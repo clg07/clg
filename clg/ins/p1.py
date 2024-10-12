fdd5fac41876cfad8615e1e68492162ec6dc9528
clg07 <vp17112002@gmail.com>
2024-10-12 12:13:06 +0530

    Update p1.py

--- clg/ins/p1.py
+++ clg/ins/p1.py
@@ -1,10 +1,12 @@
+# import substitution and transposition cipher
+
 def encrypt(text, s):
     result = ""
     for i in range(len(text)):
         char = text[i]
         if char.isupper():
             result += chr((ord(char) + s - 65) % 26 + 65)  
-            #for decryption result += chr((ord(char) - s - 65) % 26 + 65)
+
         else:
             result += chr((ord(char) + s - 97) % 26 + 97)
     return result
