71d1623b1b2ee49eedd916b168d35eb0f47ef8f5
clg07 <vp17112002@gmail.com>
2025-04-18 16:22:40 +0530

    Update p1.py

--- IR/p1.py
+++ IR/p1.py
@@ -1,9 +1,7 @@
 document1 = "The quick brown fox jumped over the lazy dog"
 document2 = " The lazy dog slept in the sun"
 
-doc1 = "apple mango banana"
-doc2 = "apple banana"
-doc3 = "apple orange"
+
 term = sorted(set((document1+document2).lower().split(" ")))
 
 for i in term:
@@ -14,33 +12,3 @@ for i in term:
     else:
         print(f"{i} -> Document 2 ({document2.lower().count(i)})")
 
-def b_and():
-    data = []
-    if "apple" in doc1 and "banana" in doc1:
-        data.append(1)
-    if "apple" in doc2 and "banana" in doc2:
-        data.append(2)
-    if "apple" in doc3 and "banana" in doc3:
-        data.append(3)
-    return data
-def b_or():
-    data = []
-    if "apple" in doc1 or "orange" in doc1:
-        data.append(1)
-    if "apple" in doc2 or "orange" in doc2:
-        data.append(2)
-    if "apple" in doc3 or "orange" in doc3:
-        data.append(3)
-    return data
-def b_not():
-    data = []
-    if "orange" not in doc1:
-        data.append(1)
-    if "orange" not in doc2:
-        data.append(2)
-    if "orange" not in doc3:
-        data.append(3)
-    return data
-print("Documents containing 'apple' AND 'banana': ",b_and())
-print("Documents containing 'apple' OR 'orange': ",b_or())
-print("Documents NOT containing 'orange': ",b_not())
