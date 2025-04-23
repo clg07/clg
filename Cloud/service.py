1689d53a5753055157407cd35c96a318c4675d51
myclg07 <vp17112002@gmail.com>
2025-04-23 20:04:17 +0530

    Delete Cloud/service.py

--- Cloud/service.py
+++
@@ -1,6 +0,0 @@
-from zeep import Client
-wsdl = "http://localhost:8080/WebApplication1/inrtoDollr?wsdl"
-cli = Client(wsdl)
-a=int(input("Enter amount in dollars : "))
-result = cli.service.INRTODOLLAR(a)
-print(result)
