4ff1dac1286fef9229d89d9f0bfd07e7c5299872
myclg07 <vp17112002@gmail.com>
2025-04-23 19:58:49 +0530

    Delete IR directory

--- IR/p4b.py
+++
@@ -1,7 +0,0 @@
-from sklearn.metrics import average_precision_score
-
-y_true = [0, 1, 1, 0, 1, 1]
-y_scores = [0.1, 0.4, 0.35, 0.8, 0.65, 0.9]
-
-avg_precision = average_precision_score(y_true, y_scores)
-print(f'Average Precision-Recall Score: {avg_precision}')
